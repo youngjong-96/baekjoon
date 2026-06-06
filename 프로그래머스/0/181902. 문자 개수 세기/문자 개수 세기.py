1ce7f110151392f921abbc063c87c32bd19fb6ef
youngjong <dudwhd96@naver.com>
2026-06-07 02:42:43 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    for letter in letters:
+        answer.append(my_string.count(letter))
+        
+    return answer
