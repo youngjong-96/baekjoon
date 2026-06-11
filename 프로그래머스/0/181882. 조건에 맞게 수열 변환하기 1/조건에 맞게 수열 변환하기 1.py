88cba475a2d91a0c40f9dd80ea2c8e274a796cf8
youngjong <dudwhd96@naver.com>
2026-06-11 12:20:53 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 57.72 ms, Memory: 42.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    answer = []
+    l = len(arr)
+    
+    for n in arr:
+        if n >= 50 and n % 2 == 0:
+            answer.append(n // 2)
+        elif n < 50 and n % 2 == 1:
+            answer.append(n * 2)
+        else:
+            answer.append(n)
+            
+    return answer
